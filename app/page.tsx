681609e0957b9e1a7a203b9be3832b876cf65189
cwebley <cameron.webley@gmail.com>
2023-01-26 00:29:46 -0600

    feat: installs prettier-plugin-tailwindcss as dev dep

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-lg py-2">Hi from Page</h1>
+      <h1 className="py-2 text-lg">Hi from Page</h1>
     </main>
   );
 }
